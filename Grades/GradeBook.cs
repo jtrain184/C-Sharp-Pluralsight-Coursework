dcd7bc06cf03e054123a4bb2e1332657998d8ccc
Bman <Bman>
2016-11-05 14:29:55 -0500

    Events and delegates

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -8,6 +8,7 @@ namespace Grades
 
 		public GradeBook()
 		{
+            _name = "Empty";
 			grades = new List<float>();
 		}
 
@@ -30,11 +31,34 @@ namespace Grades
 		public void AddGrade(float grade)
 		{
 			grades.Add(grade);
-			Console.Write(grade + " added to gradebook\n");
 		}
 
-        public string Name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if(!String.IsNullOrEmpty(value))
+                {
+                    if(_name != value)
+                    {
+                        NamedChangedEventArgs args = new NamedChangedEventArgs();
+                        args.ExistingName = _name;
+                        args.NewName = value;
 
+                        NameChanged(this, args);
+                    }
+                    
+                    _name = value;
+                }
+            }
+        }
+        public event NameChangedDelegate NameChanged;
+
+        private string _name;
 		private List<float> grades;  //Field naming convention is lower class
 	}
 }
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -13,16 +13,28 @@ namespace Grades
         {
             Console.Write("******Program now running.******\n");
             GradeBook book = new GradeBook();
+            book.NameChanged += OnNameChanged;            
+
+            book.Name = "Phil's Grade book";
+            book.Name = "Grade book";
+
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
 
             GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(book.Name);
             WriteResult("Average", stats.AverageGrade);
             WriteResult("Highest", (int)stats.HighestGrade);
             WriteResult("Lowest", (int)stats.LowestGrade);
-            Console.Write("******Program ended******");
+            Console.Write("******Program ended******\n");
         }
+
+        static void OnNameChanged(object sender, NamedChangedEventArgs args)
+        {
+            Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}");
+        }
+
         static void WriteResult(string description, int result)
         {
             Console.WriteLine(description + ": " + result);
